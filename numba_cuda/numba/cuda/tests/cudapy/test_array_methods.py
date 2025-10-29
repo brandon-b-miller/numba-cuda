56d8c68040e0585c6f2c90b96ecfe3e5e03fa9b5
brandon-b-miller <brmiller@nvidia.com>
2025-10-29 07:09:33 -0700

    initial

--- numba_cuda/numba/cuda/tests/cudapy/test_array_methods.py
+++ numba_cuda/numba/cuda/tests/cudapy/test_array_methods.py
@@ -6,6 +6,9 @@ from numba import cuda
 from numba.cuda.testing import CUDATestCase
 import unittest
 
+from numba.tests.support import TestCase, MemoryLeakMixin
+from numba.cuda import config
+
 
 def reinterpret_array_type(byte_arr, start, stop, output):
     # Tested with just one thread
@@ -32,6 +35,39 @@ class TestCudaArrayMethods(CUDATestCase):
 
             got = output[0]
             self.assertEqual(expect, got)
+
+
+class TestArrayMethods(MemoryLeakMixin, TestCase):
+    """
+    Test array reduction methods such as copy()
+    """
+
+    def setUp(self):
+        super(TestArrayMethods, self).setUp()
+        np.random.seed(42)
+        self.old_nrt_setting = config.CUDA_ENABLE_NRT
+        config.CUDA_ENABLE_NRT = True
+
+    def tearDown(self):
+        config.CUDA_ENABLE_NRT = self.old_nrt_setting
+        super(TestArrayMethods, self).tearDown()
+
+    def test_array_copy(self):
+        ary = np.array([1.0, 2.0, 3.0])
+        out = cuda.to_device(np.zeros(3))
+
+        @cuda.jit
+        def kernel(out):
+            gid = cuda.grid(1)
+            if gid < 1:
+                cpy = ary.copy()
+                for i in range(len(out)):
+                    out[i] = cpy[i]
+
+        kernel[1, 1](out)
+
+        result = out.copy_to_host()
+        np.testing.assert_array_equal(result, ary)
 
 
 if __name__ == "__main__":
